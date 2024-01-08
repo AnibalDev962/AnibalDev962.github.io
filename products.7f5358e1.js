44a3271c263fd9cef0dda6046a32370d0cc0937f
AnibalDev962 <anibalpalacios320@outlook.com>
2024-01-08 17:00:17 -0600

    testing moving to products

---
+++ products.7f5358e1.js
@@ -0,0 +1,2 @@
+let e;e=JSON.parse(localStorage.getItem("id")),document.querySelector(".texto22").textContent=e,console.log(e),localStorage.removeItem("id");
+//# sourceMappingURL=products.7f5358e1.js.map
---
+++ products.ebb34725.js
@@ -0,0 +1,2 @@
+let e=JSON.parse(localStorage.getItem("id"));document.querySelector(".texto22").textContent=e,console.log(e),localStorage.removeItem("id");
+//# sourceMappingURL=products.ebb34725.js.map
