5ac407a61723ed48f1af0407e32487bd557ab407
AnibalDev962 <anibalpalacios320@outlook.com>
2024-01-10 22:28:21 -0600

    products selector fully built

--- products.7f5358e1.js
+++
@@ -1,2 +0,0 @@
-let e;e=JSON.parse(localStorage.getItem("id")),document.querySelector(".texto22").textContent=e,console.log(e),localStorage.removeItem("id");
-//# sourceMappingURL=products.7f5358e1.js.map
--- products.ebb34725.js
+++
@@ -1,2 +0,0 @@
-let e=JSON.parse(localStorage.getItem("id"));document.querySelector(".texto22").textContent=e,console.log(e),localStorage.removeItem("id");
-//# sourceMappingURL=products.ebb34725.js.map
